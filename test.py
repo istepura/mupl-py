e6879d7742ac6a71784a449ea45be8fe341fffcd
Igor Stepura <igor.stepura@gmail.com>
2013-10-08 16:11:54 +0900

    Use unittest

--- test.py
+++ test.py
@@ -1,45 +1,83 @@
+import unittest
 from lexer import *
 from parser import *
 
-def main():
-    l = Lexer()
-    l.init("(add (int 3) (int 8))")
+class ParserTest(unittest.TestCase):
+    def setUp(self):
+        self._parser = Parser(Lexer())
+    def parseToStr(self, string):
+        ast = self._parser.parse(string)
+        res = ast.eval({})
+        return str(res)
 
-    s = l.process()
+class TestBasic(ParserTest):
+    def testPositiveInt(self):
+        res = self.parseToStr("(int 3)")
+        self.assertEqual(res, '(int 3)')
 
-    lex = Lexer()
-    sc = Parser(lex)
+    def testPositiveLongInt(self):
+        s = "(int 12345678903)"
+        res = self.parseToStr(s)
+        self.assertEqual(res, s)
 
-    ast = sc.parse("(add (int 3)    (int    8)  )")
-    print ast
+    def testNegativeInt(self):
+        s = "(int -1)"
+        res = self.parseToStr(s)
+        self.assertEqual(res, s)
 
-    print ast.eval({})
+    def testLongNegativeInt(self):
+        s = "(int -12345678903)"
+        res = self.parseToStr(s)
+        self.assertEqual(res, s)
 
-    s2 = '''(mlet "f1" (fun "f1" "a" (mlet "x" (var "a") (fun "f2" "z" (add (var "x") (int 1)))))
+    def testAunit(self):
+        res = self.parseToStr("(aunit)")
+        self.assertEqual(res, "(aunit)")
+
+    def testBadInt(self):
+        self.assertRaises(SyntaxError, self.parseToStr, "int 3")
+
+    def testBadInt2(self):
+        self.assertRaises(SyntaxError, self.parseToStr, '(int "3")')
+
+class TestAdd(ParserTest):
+    def test1(self):
+        s ="(add (int 3) (int 8))"
+        res = self.parseToStr(s)
+        self.assertEqual(res, "(int 11)")
+
+    def test2(self):
+        s = "(add (int 300)    (int    -98)  )"
+        res = self.parseToStr(s)
+        self.assertEqual(res, "(int 202)")
+
+
+class TestComplex(ParserTest):
+    def test1(self):
+        s = '''(mlet "f1" (fun "f1" "a" (mlet "x" (var "a") (fun "f2" "z" (add (var "x") (int 1)))))
                       (mlet "f3" (fun "f3" "f" (mlet "x" (int 1729) (call (var "f") (aunit))))
                                      (call (var "f3") (call (var "f1") (int 1))))) '''
-    l.init(s2)
+        res = self.parseToStr(s)
+        self.assertEqual(res, "(int 2)")
+    def test2(self):
+        s = '(call (fun "incr" "x" (add (var "x") (int 1))) (int 42)))'
+        res = self.parseToStr(s)
+        self.assertEqual(res, "(int 43)")
 
-    ast = sc.parse(s2)
-    print ast
-    print ast.eval({})
+    def test3(self):
+        s = '(ifgreater (add (int 0)(int 1)) (add (int 0)(int 2)) (int 3) (int 4))'
+        res = self.parseToStr(s)
+        self.assertEqual(res, "(int 4)")
 
-    simplestr = '(call (fun "incr" "x" (add (var "x") (int 1))) (int 42)))'
-    ast = sc.parse(simplestr)
-    print ast.eval({})
+    def test4(self):
+        s = '(ifgreater (add (int 0)(int 2)) (add (int 1) (int 0)) (int 3) (int 4))'
+        res = self.parseToStr(s)
+        self.assertEqual(res, "(int 3)")
 
-    s = '(ifgreater (add (int 0)(int 1)) (add (int 0)(int 2)) (int 3) (int 4))'
-    ast = sc.parse(s)
-    print ast.eval({})
+    def test4(self):
+        s= '(mlet "fnc"       (fun "f1" "x"             (ifgreater (isaunit (var "x")) (int 0)                        (int 0)                        (add (fst (var "x")) (call (var "f1") (snd (var "x"))))))       (call (var "fnc") (apair (int 1) (apair (int 2) (apair (int 3) (aunit))))))' 
+        res = self.parseToStr(s)
+        self.assertEqual(res, "(int 6)")
 
-    s = '(ifgreater (add (int 0)(int 2)) (add (int 1) (int 0)) (int 3) (int 4))'
-    ast = sc.parse(s)
-    print ast.eval({})
-
-    s= '(mlet "fnc"       (fun "f1" "x"             (ifgreater (isaunit (var "x")) (int 0)                        (int 0)                        (add (fst (var "x")) (call (var "f1") (snd (var "x"))))))       (call (var "fnc") (apair (int 1) (apair (int 2) (apair (int 3) (aunit))))))' 
-
-    ast = sc.parse(s)
-    print ast
-    print ast.eval({})
 if __name__ == "__main__":
-    main() 
+    unittest.main()
